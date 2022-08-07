d3e635e7e3c290f3a5ea5440a485bd23fea1b047
Aykut C. Satici <acsatici@gmail.com>
2022-08-07 14:59:56 -0600

    started factor graphs

---
+++ Julia/testfactors.jl
@@ -0,0 +1,12 @@
+include("discfactors.jl")
+
+X = Variable(:x, 2)
+Y = Variable(:y, 2)
+Z = Variable(:z, 2)
+
+ϕ = Factor([X, Y, Z], FactorTable(
+    (x=1, y=1, z=1) => 0.08, (x=1, y=1, z=2) => 0.31,
+    (x=1, y=2, z=1) => 0.09, (x=1, y=2, z=2) => 0.37,
+    (x=2, y=1, z=1) => 0.01, (x=2, y=1, z=2) => 0.05,
+    (x=2, y=2, z=1) => 0.02, (x=2, y=2, z=2) => 0.07,
+))
